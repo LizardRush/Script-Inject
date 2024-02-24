f6224a544e144f53e3974ada0ece9d241e2c5b0a
Lizard Rush <151949502+LizardRush@users.noreply.github.com>
2024-02-24 17:54:53 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,52 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var openTabAndExecuteButton = document.getElementById('unsubAllChannels');
 
+  openTabAndExecuteButton.addEventListener('click', function() {
+    chrome.tabs.create({ url: "https://www.youtube.com/feed/channels" }, function(tab) {
+      setTimeout(function() {
+        chrome.tabs.executeScript(tab.id, {
+          code: `
+            (async function iife() {
+              var UNSUBSCRIBE_DELAY_TIME = 500;
+              var runAfterDelay = (fn, delay) => new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  fn();
+                  resolve();
+                }, delay);
+              });
 
-(async function iife() {
+              var channels = Array.from(document.getElementsByTagName('ytd-channel-renderer'));
+              console.log(\`\${channels.length} channels found.\`);
 
-
-
-var UNSUBSCRIBE_DELAY_TIME = 500
-
-
-
-var runAfterDelay = (fn, delay) => new Promise((resolve, reject) => {
-
-setTimeout(() => {
-
-fn()
-
-resolve()
-
-}, delay)
-
-})
-
-var channels = Array.from(document.getElementsByTagName(`ytd-channel-renderer`))
-
-console.log(`${channels.length} channels found.`)
-
-var ctr = 0
-
-for (const channel of channels) {
-
-
-
-channel.querySelector(`[aria-label^='Unsubscribe from']`).click()
-
-await runAfterDelay(() => {
-
-
-
-document.getElementsByTagName(`yt-confirm-dialog-renderer`)[0]
-
-
-.querySelector(`[aria-label^='Unsubscribe']`).click()
-
-console.log(`Unsubsribed ${ctr + 1}/${channels.length}`)
-
-ctr++
-
-}, UNSUBSCRIBE_DELAY_TIME)
-
-}
-
-})()
+              var ctr = 0;
+              for (const channel of channels) {
+                channel.querySelector('[aria-label^="Unsubscribe from"]').click();
+                await runAfterDelay(async () => {
+                  await runAfterDelay(() => {
+                    document.querySelector('yt-confirm-dialog-renderer paper-button#confirm-button').click();
+                    console.log(\`Unsubscribed \${ctr + 1}/\${channels.length}\`);
+                    ctr++;
+                  }, UNSUBSCRIBE_DELAY_TIME);
+                }, UNSUBSCRIBE_DELAY_TIME);
+              }
+            })();
+          `
+        });
+      }, 10000); // 10 seconds delay
+    });
+  });
+});
